d8d991fb38d85f947353c8fbc3eeae65c92b778b
Nicholas Buhay <nbuhay@gmail.com>
2016-06-08 16:00:23 +0000

    Add about page and default header and footer.

--- webserver/controllers/indexCtrl.js
+++ webserver/controllers/indexCtrl.js
@@ -1,6 +1,11 @@
 var express = require('express');
 
-/* GET home page. */
+/* GET home page */
 module.exports.home = function(req, res) {
-  res.render('index', { title: 'Test' });
+  res.render('index', { title: 'Hyoshi', subTitle: 'Natural Rhythm' });
+};
+
+/* GET about page */
+module.exports.about = function(req, res) {
+    res.render('about', {title: 'About'});
 };
--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var indexCtrl = require('../controllers/indexCtrl');
 
-/* GET home page. */
+/* GET home page */
 router.get('/', indexCtrl.home);
+
+/*GET about page */
+router.get('/about', indexCtrl.about);
 
 module.exports = router;
