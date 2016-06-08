4cde66ff4cdb3c2a6e184c1bfd99dbb6ce1260aa
Nicholas Buhay <nbuhay@gmail.com>
2016-06-08 17:15:35 +0000

    Complete basic login, about, and home page layout.

--- webserver/controllers/indexCtrl.js
+++ webserver/controllers/indexCtrl.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 /* GET home page */
 module.exports.home = function(req, res) {
-  res.render('index', { title: 'Hyoshi', subTitle: 'Natural Rhythm' });
+  res.render('index', { title: 'Welcome', subTitle: 'Hyoshi - Natural Rhythm' });
 };
 
 /* GET about page */
