9c24983fd3a3f1bfe16f7dd288fb174eb6167364
Nicholas Buhay <nbuhay@gmail.com>
2016-06-09 20:05:27 +0000

    MongoDB initial integration using Mongoose.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('./webserver/models/db');
 
 var routes = require('./webserver/routes/index');
 var users = require('./webserver/routes/users');
