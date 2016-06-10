e5c25db31a476c2a133a70034a98dd87852baf8c
Nicholas Buhay <nbuhay@gmail.com>
2016-06-10 20:00:50 +0000

    Begin users schema.

--- webserver/models/users.js
+++ webserver/models/users.js
@@ -1,3 +1,21 @@
 var mongoose = require('mongoose');
 
-var userSchema = new mongoose.Schema({});
+var userSchema = new mongoose.Schema({
+    picture: String,
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: Number,
+        required: true
+    },
+    instruments: ['String'],
+    friends: ['String'],
+    jamHistory: ['String'],
+    favoriteAlbums: ['String']
+});
