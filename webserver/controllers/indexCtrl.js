a2edcc0b04a377cb59b435843be9fe5dbc01073a
Nicholas Buhay <nbuhay@gmail.com>
2016-06-13 19:13:02 +0000

    Create new Date object for Jam Listing times on User homepage.

--- webserver/controllers/indexCtrl.js
+++ webserver/controllers/indexCtrl.js
@@ -11,7 +11,7 @@ module.exports.home = function(req, res) {
         {
           goal: 'Songwriting',
           host: 'Nicholas',
-          time: '1465842437',
+          time: new Date('1465842437'),
           geoLocation: {
             type: 'point',
             coordinates: ['-82.379457', '28.027905']
@@ -23,7 +23,7 @@ module.exports.home = function(req, res) {
         {
           goal: 'Practice',
           host: 'Aaron',
-          time: '1465842479',
+          time: new Date('1465842479'),
           geoLocation: {
             type: 'point',
             coordinates: ['-82.400372', '28.018943']
@@ -34,7 +34,7 @@ module.exports.home = function(req, res) {
         {
           goal: 'Blues Jam',
           host: 'Bill',
-          time: '1465842503',
+          time: new Date('1465842503'),
           geoLocation: {
             type: 'point',
             coordinates: ['-82.382605', '28.043523']
