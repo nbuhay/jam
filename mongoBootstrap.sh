a583ac331f13332a174dd52f3951a89675accb06
Nicholas Buhay <nbuhay@gmail.com>
2016-10-02 09:49:14 -0400

    Fix importing user collection

--- mongoBootstrap.sh
+++ mongoBootstrap.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-declare -a users=(Nicholas Aaron Ant)
-declare -a instruments=('electric guitar' organ 'acoustic guitar')
-declare -a questions=('Quarter note or quaiver?' 'Do you identify more as major or minor?  Why?' 'Why do you play your instrument?')
+declare -a users=('Nicholas' 'Aaron' 'Ant')
+declare -a instruments=('electric guitar' 'organ' 'acoustic guitar')
 DB=Hyoshi
 
 mongoimport --db Hyoshi --collection users --drop --file ./mockData/users.json
 mongoimport --db Hyoshi --collection instruments --drop --file ./mockData/instruments.json
-mongoimport --db Hyoshi --collection iceBreakerQuestions --drop --file ./mockData/iceBreakerQuestions.json
 
 # Assign each user an instrument
 TOTAL=${#users[@]}
--- webserver/models/db.js
+++ webserver/models/db.js
@@ -42,4 +42,3 @@ process.on('SIGTERM', function() {
 require('./users');
 require('./jamLocations');
 require('./instruments');
-require('./iceBreakerQuestions');
